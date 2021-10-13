d2518ab0e7f2dd9f153d5389865c8ff05b696065
Paul Brabban <paul.brabban@gmail.com>
2021-10-13 10:01:52 +0100

    finish off stub for XML interactions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import defusedxml.ElementTree as ET
 
 
-def is_valid_post(post):
-    return post['']
+def to_sql(post):
+    return (post.attrib['Id'], post.attrib['PostTypeId'], post.attrib['CreationDate'])
 
 
-with open('uncommitted/Posts.xml', 'r') as badges_in:
-    tree = ET.parse(badges_in)
-    print([elem.attrib for elem in tree.getroot()][:10])
+with open('uncommitted/Posts.xml', 'r') as posts_in:
+    tree = ET.parse(posts_in)
+    print([to_sql(elem) for elem in tree.getroot()][:10])
